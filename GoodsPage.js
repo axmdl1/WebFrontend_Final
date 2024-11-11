433775fb5739a56f2a49ea09bfea23e913d7406f
axmdl1 <mr.akhmedali@bk.ru>
2024-11-11 15:50:10 +0500

    Deleted white space, need to finish

--- GoodsPage.js
+++ GoodsPage.js
@@ -1,26 +1,5 @@
-/*
-const boxes = document.querySelectorAll('.elements');
-
-window.addEventListener('scroll', checkBoxes);
-
-checkBoxes();
-
-function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4;
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
-
-        if (boxTop < triggerBottom) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show');
-        }
-    })
-}
-*/
-
 AOS.init({
     duration: 1200,
     once: false,
-    easing: 'ease-in-out',
+    //easing: 'ease-in-out',
 });
