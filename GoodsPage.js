74cc105c57efc12ef29fe8f48fd0d47c3ccaecdd
axmdl1 <mr.akhmedali@bk.ru>
2024-11-12 01:13:58 +0500

    finished goods

--- GoodsPage.js
+++ GoodsPage.js
@@ -1,5 +1,10 @@
 AOS.init({
     duration: 1200,
     once: false,
-    //easing: 'ease-in-out',
+    easing: 'ease-in-out',
 });
+
+function scrollDown(){
+    var elem = document.getElementById('elements');
+    elem.scrollIntoView({behavior: 'smooth'});
+}
